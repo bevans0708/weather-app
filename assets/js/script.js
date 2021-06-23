5cc581430e7b0b52e0fa9edbcdcf08aa5e3502b4
Brandon Evans <brandon.evans0708@gmail.com>
2021-06-22 18:27:13 -0600

    added new API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var appKey = "dfd0be5259f9413b83c173406211906"
 var searchMethod;
 
-(fetch('https://api.weatherapi.com/v1/current.json?key=dfd0be5259f9413b83c173406211906&q=London&aqi=no'))
+(fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=ee6ddb4a548859b074fe9d14d14de471'))
    .then(response)
    .then(data => console.log(data))
