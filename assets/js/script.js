73c07ed986fed49b3dc97ef6755960aaaa865530
Brandon Evans <brandon.evans0708@gmail.com>
2021-06-22 19:03:59 -0600

    worked on fetch getting api to pull display data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-var appKey = "dfd0be5259f9413b83c173406211906"
+var appKey = "ee6ddb4a548859b074fe9d14d14de471"
 var searchMethod;
 
-(fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=ee6ddb4a548859b074fe9d14d14de471'))
-   .then(response)
-   .then(data => console.log(data))
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + appKey, function(data) {
+   console.log(data);
+});
+
+// (fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=ee6ddb4a548859b074fe9d14d14de471'))
+//    .then(response)
+//    .then(data => console.log(data))
