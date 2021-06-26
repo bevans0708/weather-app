2f5d6efb55d7e827fc543a4ef7a924731078a195
Brandon Evans <brandon.evans0708@gmail.com>
2021-06-26 14:11:04 -0600

    got both fetch calls to work together

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,53 @@
 var searchBtn = document.getElementById('searchBtn')
 var searchInput = document.getElementById('searchInput')
 var appKey = "ee6ddb4a548859b074fe9d14d14de471"
-var searchMethod;
-var lat = "";
-var lon = "";
+var cardContainer = document.getElementById('cardContainer')
+var forecastOne = document.getElementById('forecastCard')
+var lat;
+var lon;
 
 
+// function geoCode() {
+//    var geoCodeData;
+//    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&appid=" + appKey
+//    ).then(function(response) {
+//       if (response.ok) {
+//          return response.json();
+//       } else {
+//          return promise.reject(response);
+//       }
+//    }).then(function (data) {
+//       // geoCodeData = data;
+//       console.log(data);
+//       return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.lat + "&lon=" + data.lon + "&units=imperial&appid=" + appKey)
+//    }).then(function(response) {
+//       if (response.ok) {
+//          return response.json();
+//       } else {
+//          return promise.reject(response);
+//       }
+//    }).then(function(weatherData) {
+//       // console.log(geoCodeData);
+//       console.log(weatherData);
+//    }).catch(function (error) {
+//       console.warn(error);
+//    })
+// };
 
-function geoCode() {
-   $.getJSON("http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput.value +"&appid=" + appKey, function(data) {
-      var latPull = data[0].lat;
-      var lonPull = data[0].lon;
-      window.lat = latPull;
-      window.lon = lonPull;
+// $.getJSON("http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput.value +"&appid=" + appKey, function(data) {
+//    var geoData;
+//    geoData = data
+//    console.log(geoData)
 
-      console.log(lat);
-      console.log(lon);
-      console.log(data)
-   })
-}
+//    return $.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=51.5085&lon=-0.1257&appid=" + appKey, function(response) {
+//       console.log(response);
+//    });
+
+// var day = moment.unix(1624734000);
+// console.log(day);
 
 function citySearch() {
-   $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&units=imperial&appid=" + appKey, function (data) {
+      $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&units=imperial&appid=" + appKey, function (data) {
       searchInput.value = "";
       var cityName = data.name
       var temp = Math.floor(data.main.temp) + " \u00B0F";
@@ -29,8 +55,10 @@ function citySearch() {
       var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
       var wind = Math.floor(data.wind.speed) + " mi/hr"
       var humidity = data.main.humidity + " %"
+      lat = data.coord.lat;
+      lon = data.coord.lon;
       
-      console.log(data);
+      console.log(lat, lon);
       
       $("#cityHeader").append(cityName);
       $("#temperature").append(temp);
@@ -38,17 +66,32 @@ function citySearch() {
       $("#iconImg").attr("src", icon);
       $("#windSpeed").append(wind);
       $("#humidity").append(humidity);
-      // $("#uvIndex").append(uv);
       
+      $.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + appKey, function (data) {
+         console.log(data);
+         var uv = data.current.uvi
+         // var dayConvert = moment.unix(data.daily[1].dt);
+         // var convertedTime = moment().format("MMM Do YYYY");
+         // console.log(dayConvert);
+         // console.log(convertedTime);
+         var datesArr = data.daily
+         console.log(datesArr[2]);
+         
+         // $("#uvIndex").append(uv);
+         // $("#day-1").append(converted);
+         // $("#uvIndex").append(uv);
+         // $("#uvIndex").append(uv);
+
+         });
    });
    
-   $.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat.value + "&lon=" + lon.value + "&appid=" + appKey, function (data) {
-         console.log(data);
-      });
-   }
+}
+
+// function oneCall () {
+// }
    
-   
-   searchBtn.addEventListener('click', citySearch);
-   searchBtn.addEventListener('click', geoCode);
-   
-   
+   searchBtn.addEventListener('click', function() {
+
+      citySearch();
+   })
+// searchBtn.addEventListener('click', geoCode)
